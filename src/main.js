2da7c3e19a3190e7abe7f104473fca5d470553f3
Tzihual <129258298+Tzihual@users.noreply.github.com>
2024-07-03 10:25:14 -0700

    menú principal

--- src/main.js
+++ src/main.js
@@ -1,30 +1,31 @@
 // src/main.js
-const { app, BrowserWindow } = require('electron');
+const path = require('path')
 
-function createWindow() {
+const { app, BrowserWindow } = require('electron')
+
+function createWindow () {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      nodeIntegration: true
+      nodeIntegration: true,
+      preload: path.join(__dirname, 'preload.js')
     }
-  });
+  })
 
-  win.loadFile('src/index.html');
+  win.loadFile('src/index.html')
 }
 
-app.whenReady().then(createWindow);
+app.whenReady().then(createWindow)
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
-    app.quit();
+    app.quit()
   }
-});
+})
 
 app.on('activate', () => {
   if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
+    createWindow()
   }
-});
-
-
+})
